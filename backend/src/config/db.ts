9215b7587b3b67b6067184e08058e66799825314
Shubham kahar <Shubhamkahar196@gmail.com>
2025-10-16 22:51:18 +0530

    Working api

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '../../dist/generated/prisma/client.js'
 
 const client = new PrismaClient();
 
--- backend/src/controllers/placesController.ts
+++ backend/src/controllers/placesController.ts
@@ -1,19 +1,22 @@
-import { type Request, type Response} from "express";
+import {type  Request, type Response } from "express";
 import client from "../config/db.js";
 
-export const getPlaces = async (req: Request,res : Response)=>{
-    const {category, time } = req.body;
-    try {
-        const places = await client.place.findMany({
-            where: {
-                category: category ? String(category) : undefined,
-                bestTime: time ? String(time) : undefined,
-            },
-        });
-        res.json(places);
-    } catch (error) {
-        res.status(500).json({
-            error: 'Server error'
-        })
-    }
-}
+export const getPlaces = async (req: Request, res: Response) => {
+  const { category, time } = req.query;
+
+  try {
+    const filters: any = {};
+
+    if (category) filters.category = String(category);
+    if (time) filters.bestTime = String(time);
+
+    const places = await client.place.findMany({
+      where: filters,
+    });
+
+    res.json(places);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server Error" });
+  }
+};
